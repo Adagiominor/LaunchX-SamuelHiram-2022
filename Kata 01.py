2ec1dc06be66c7d8efe02ec36c078e667e987666
Samuel Hiram Castro Martinez <77934173+Adagiominor@users.noreply.github.com>
2022-02-20 19:22:09 -0800

    Add files via upload

---
+++ Kata 01.py
@@ -0,0 +1,9 @@
+from datetime import date
+
+print("Today's date is: {}". format(date.today()))
+
+#siendo parsec = 3.26156 lightyears realizar un conversor...
+
+parsec = input("parsec --> lighyears = ");
+
+print(str(parsec) + " parsec = " + str(int(parsec) * 3.261526 ) + " lightyears")
